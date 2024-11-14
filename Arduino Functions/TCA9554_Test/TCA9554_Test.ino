19b65eddbeecd62499ec0b3db2ec3a5890a108e6
bobby1321 <36699941+bobby1321@users.noreply.github.com>
2024-11-13 20:45:30 -0500

    Test code and changed diode for resistor
    The diode after the op-amp on the relay board is now a 10K pullup resistor

---
+++ Arduino Functions/TCA9554_Test/TCA9554_Test.ino
@@ -0,0 +1,41 @@
+#include <Wire.h>
+
+void setup() {
+  Serial.begin(115200);
+  Wire.begin();
+  delay(100);
+  Wire.beginTransmission(33);
+  Wire.write(3);
+  Wire.write(0);
+  Serial.println(String(Wire.endTransmission()));
+  delay(100);
+  Wire.beginTransmission(33);
+  Wire.write(1);
+  Wire.write(0);
+  Serial.println(String(Wire.endTransmission()));
+}
+
+void loop() {
+  
+  // for (int pin = 0; pin < 8; pin++) {
+  //   Wire.beginTransmission(33);;
+  //   Wire.write(1);
+  //   Wire.write(1<<pin);
+  //   Wire.endTransmission();
+  //   delay(100);
+  // }
+  Wire.beginTransmission(33);
+  int output_reg;
+  Wire.write(1);
+    
+  Wire.requestFrom(33, 1);
+  while (Wire.available()) 
+  {                                   
+    output_reg = Wire.read();
+    // output_reg = 0;
+    // Serial.println(String(output_reg));
+  }
+  Serial.println(String(output_reg));
+  Serial.println(String(Wire.endTransmission()));
+  delay(100);
+}
